ab4c676a0f25262ac64f7b70c1f706ace43da83d
Jay <jayvekariya2003@gmail.com>
2024-10-08 09:44:09 +0530

    Refactor Projects component to dynamically render project cards from a list

--- My-Portfolio/src/Components/Projects.jsx
+++ My-Portfolio/src/Components/Projects.jsx
@@ -10,6 +10,44 @@ import { Card, CardBody, CardFooter } from "@chakra-ui/react";
 import React from "react";
 
 const Projects = () => {
+  const List_Projects = [
+    {
+      id: 1,
+      Title: "E-commerce-website",
+      Image: "./Images/Project_1.jpg",
+      Link: "https://e-commerce-website-cikv.onrender.com/",
+      Button: "View site",
+    },
+    {
+      id: 2,
+      Title: "Online Course Registration",
+      Image: "./Images/Project_3.jpg",
+      Link: "https://online-course-registration.onrender.com/",
+      Button: "View site",
+    },
+    {
+      id: 3,
+      Title: "Unit Converter",
+      Image: "./Images/Project_2.jpg",
+      Link: "https://cute-madeleine-99cc4b.netlify.app/",
+      Button: "View site",
+    },
+    {
+      id: 4,
+      Title: "Dice Game",
+      Image: "./Images/Project_4.jpg",
+      Link: "https://testrepo2-zysh.onrender.com/",
+      Button: "Play it",
+    },
+    {
+      id: 5,
+      Title: "Image Gallery",
+      Image: "./Images/Project_5.png",
+      Link: "https://react-photo-gallery-assignment.onrender.com",
+      Button: "View site",
+    },
+  ];
+
   return (
     <div className="p-4" id="Projects">
       <Center>
@@ -22,31 +60,33 @@ const Projects = () => {
           flexWrap="wrap"
           placeContent="center"
         >
-          <Card maxW="sm" mt="8">
-            <CardBody>
-              <Image
-                src="./Images/Project_1.jpg"
-                alt="Green double couch with wooden legs"
-                borderRadius="lg"
-              />
-              <Heading size="md" paddingTop={3}>
-                <Center>E-commerce-website</Center>
-              </Heading>
-            </CardBody>
-            <Divider />
-            <CardFooter placeContent="center">
-              <Button variant="solid" colorScheme="blue">
-                <a
-                  href="https://e-commerce-website-cikv.onrender.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View site
-                </a>
-              </Button>
-            </CardFooter>
-          </Card>
-          <Card maxW="sm" mt="8">
+          {List_Projects.map((projects) => (
+            <Card maxW="sm" mt="8">
+              <CardBody>
+                <Image
+                  src={projects.Image}
+                  alt="Green double couch with wooden legs"
+                  borderRadius="lg"
+                />
+                <Heading size="md" paddingTop={3}>
+                  <Center>{projects.Title}</Center>
+                </Heading>
+              </CardBody>
+              <Divider />
+              <CardFooter placeContent="center">
+                <Button variant="solid" colorScheme="blue">
+                  <a
+                    href={projects.Link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {projects.Button}
+                  </a>
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+          {/* <Card maxW="sm" mt="8">
             <CardBody>
               <Image
                 src="./Images/Project_3.jpg"
@@ -117,7 +157,7 @@ const Projects = () => {
                 </a>
               </Button>
             </CardFooter>
-          </Card>
+          </Card> */}
         </Flex>
       </Center>
     </div>
