90035a77e9527dcd2085639a08a2fa109e2694f2
Jay <jayvekariya2003@gmail.com>
2024-10-16 16:54:55 +0530

    Refactor Projects component to dynamically render project cards from a list and add a new project

--- My-Portfolio/src/Components/Projects.jsx
+++ My-Portfolio/src/Components/Projects.jsx
@@ -46,6 +46,13 @@ const Projects = () => {
       Link: "https://react-photo-gallery-assignment.onrender.com",
       Button: "View site",
     },
+    {
+      id: 6,
+      Title: "Todo List App",
+      Image: "./Images/todo_List_app.jpg",
+      Link: "https://todo-list-app-ta64.onrender.com/",
+      Button: "View site",
+    }
   ];
 
   return (
@@ -86,78 +93,6 @@ const Projects = () => {
               </CardFooter>
             </Card>
           ))}
-          {/* <Card maxW="sm" mt="8">
-            <CardBody>
-              <Image
-                src="./Images/Project_3.jpg"
-                alt="Green double couch with wooden legs"
-                borderRadius="lg"
-              />
-              <Heading size="md" paddingTop={3}>
-                <Center>Online Course Registration</Center>
-              </Heading>
-            </CardBody>
-            <Divider />
-            <CardFooter placeContent="center">
-              <Button variant="solid" colorScheme="blue">
-                <a
-                  href="https://online-course-registration.onrender.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View site
-                </a>
-              </Button>
-            </CardFooter>
-          </Card>
-          <Card maxW="sm" mt="8">
-            <CardBody>
-              <Image
-                src="./Images/Project_2.jpg"
-                alt="Green double couch with wooden legs"
-                borderRadius="lg"
-              />
-              <Heading size="md" paddingTop={3}>
-                <Center>Unit Converter</Center>
-              </Heading>
-            </CardBody>
-            <Divider />
-            <CardFooter placeContent="center">
-              <Button variant="solid" colorScheme="blue">
-                <a
-                  href="https://cute-madeleine-99cc4b.netlify.app/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View site
-                </a>
-              </Button>
-            </CardFooter>
-          </Card>
-          <Card maxW="sm" mt="8">
-            <CardBody>
-              <Image
-                src="./Images/Project_4.jpg"
-                alt="Green double couch with wooden legs"
-                borderRadius="lg"
-              />
-              <Heading size="md" paddingTop={3}>
-                <Center>Dice Game</Center>
-              </Heading>
-            </CardBody>
-            <Divider />
-            <CardFooter placeContent="center">
-              <Button variant="solid" colorScheme="blue">
-                <a
-                  href="https://testrepo2-zysh.onrender.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Play it
-                </a>
-              </Button>
-            </CardFooter>
-          </Card> */}
         </Flex>
       </Center>
     </div>
